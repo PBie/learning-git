e6d91157d759b1bcf9210570ad81ef182191f1af
Piotr Bielski <bielski.pl@gmail.com>
2023-09-30 10:48:03 +0200

    Add loop and upper case for keys and values

--- shopping_list.py
+++ shopping_list.py
@@ -2,3 +2,15 @@ zakupy = {
     "piekarnia": ["chleb", "pączek", "bułki"],
     "warzywniak": ["marchew", "seler", "rukola"]
 }
+
+liczba_produktow = 0
+
+print("Lista zakupów")
+
+for sklep, produkty in zakupy.items():
+    sklep = sklep.upper()
+    produkty = [produkt.capitalize() for produkt in produkty]
+    
+    print(f"Idę do {sklep} i kupuję tam: {produkty}.")
+
+  
